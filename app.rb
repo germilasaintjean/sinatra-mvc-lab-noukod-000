756d8e3d152aafbc09b0d4546d7327713b91c95e
Saint-Jean Germila <germila@noukod.com>
2019-01-16 18:17:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/'do
-  erb :user_phrase
+    erb :user_input
   end
 
   post '/piglatinize' do
      user_word = params["user_phrase"]
      word = PigLatinizer.new
      @pig_latinized_word = word.to_pig_latin(user_word)
- 
- erb :pig
+
+    erb :pig
 end
