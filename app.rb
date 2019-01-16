9aac70b2b85c7cb48ccd72fa4b9439eeba9c5859
Saint-Jean Germila <germila@noukod.com>
2019-01-16 16:35:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/'do
+  erb :user_phrase
+  end
+
+  post '/piglatinize' do
+  # @phrase = params["phrase"]
+  # @up = params["up"]
+  end
+
+  post '/piglatinize' do
+
+  end
 end
